7af6aab9799c98fae75b97c62107c5ed9db2bf5c
root <root@core-ssh.local.hass.io>
2020-06-06 19:29:54 +0100

    Added SonoffLAN and new pngs

--- custom_components/sonoff/switch.py
+++ custom_components/sonoff/switch.py
@@ -1,145 +1,107 @@
-import logging, time, json
+"""
+Firmware   | LAN type  | uiid | Product Model
+-----------|-----------|------|--------------
+ITA-GZ1-GL | plug      | 14   | Sonoff (Sonoff Basic)
+PSF-B01-GL | plug      | 1    | - (switch 1ch)
+PSF-BD1-GL | plug      | 1    | MINI (Sonoff Mini)
+PSF-B04-GL | strip     | 2    | - (switch 2ch)
+PSF-B04-GL | strip     | 4    | 4CHPRO (Sonoff 4CH Pro)
+"""
+import logging
+from typing import Optional
 
-from homeassistant.components.switch import SwitchDevice
-from homeassistant.components.switch import DOMAIN
-# from homeassistant.components.sonoff import (DOMAIN as SONOFF_DOMAIN, SonoffDevice)
-from custom_components.sonoff import (DOMAIN as SONOFF_DOMAIN, SonoffDevice)
+from homeassistant.helpers.entity import ToggleEntity
+
+# noinspection PyUnresolvedReferences
+from . import DOMAIN, CONF_FORCE_UPDATE, SCAN_INTERVAL
+from .sonoff_main import EWeLinkDevice
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Add the Sonoff Switch entities"""
 
-    entities = []
-    for device in hass.data[SONOFF_DOMAIN].get_devices(force_update = True):
-        outlets_number = hass.data[SONOFF_DOMAIN].get_outlets(device)
+async def async_setup_platform(hass, config, add_entities,
+                               discovery_info=None):
+    if discovery_info is None:
+        return
 
-        if outlets_number is None: # fallback to whatever the device might have
-            if 'switches' in device['params']: # the device has multiple switches, split them by outlets
-                for outlet in device['params']['switches']:
-                    entity = SonoffSwitch(hass, device, outlet['outlet'])
-                    entities.append(entity)
+    deviceid = discovery_info['deviceid']
+    channels = discovery_info['channels']
+    registry = hass.data[DOMAIN]
+    add_entities([EWeLinkToggle(registry, deviceid, channels)])
 
-            elif 'switch' in device['params'] or 'state' in device['params']:
-                entity = SonoffSwitch(hass, device)
-                entities.append(entity)
 
-        elif outlets_number > 1: # the device has multiple switches, split them by available outlets
-            for outlet in range(0, outlets_number):
-                entity = SonoffSwitch(hass, device, outlet)
-                entities.append(entity)
+class EWeLinkToggle(ToggleEntity, EWeLinkDevice):
+    """Toggle can force update device with sledonline command."""
+    _should_poll = None
+    _sled_online = None
 
-        # normal device = Sonoff Basic (and alike)
-        elif 'switch' in device['params'] or 'state' in device['params']: #ignore devices like Sonoff RF bridge:
-            entity = SonoffSwitch(hass, device)
-            entities.append(entity)
+    async def async_added_to_hass(self) -> None:
+        device = self._init()
+        self._should_poll = device.pop(CONF_FORCE_UPDATE, False)
 
-    if hass.data[SONOFF_DOMAIN].get_debug_state():
-        debug_entity = SonoffDebugSwitch(hass)
-        entities.append(debug_entity)
+    def _update_handler(self, state: dict, attrs: dict):
+        # TH bug in local mode https://github.com/AlexxIT/SonoffLAN/issues/110
+        if not (self._is_th_3_4_0 and attrs.get('temperature') == 0 and
+                attrs.get('humidity') == 0):
+            self._attrs.update(attrs)
 
-    if len(entities):
-        async_add_entities(entities, update_before_add=False)
+        if 'switch' in state or 'switches' in state:
+            self._is_on = any(self._is_on_list(state))
 
-class SonoffSwitch(SonoffDevice, SwitchDevice):
-    """Representation of a Sonoff switch device."""
+        if 'sledOnline' in state:
+            self._sled_online = state['sledOnline']
 
-    def __init__(self, hass, device, outlet = None):
-        """Initialize the device."""
+        self.schedule_update_ha_state()
 
-        # add switch unique stuff here if needed
-        SonoffDevice.__init__(self, hass, device)
-        self._outlet = outlet
-        self._name   = '{}{}'.format(device['name'], '' if outlet is None else ' '+str(outlet+1))
+    @property
+    def should_poll(self) -> bool:
+        # The device itself sends an update of its status
+        return self._should_poll
 
-        if outlet is None:
-            self._name      = device['name']
-
+    @property
+    def unique_id(self) -> Optional[str]:
+        if self.channels:
+            chid = ''.join(str(ch) for ch in self.channels)
+            return f'{self.deviceid}_{chid}'
         else:
-            self._attributes['outlet'] = outlet
-
-            if 'tags' in device and 'ck_channel_name' in device['tags']:
-                if str(outlet) in device['tags']['ck_channel_name'].keys() and \
-                    device['tags']['ck_channel_name'][str(outlet)]:
-                    self._name = '{} - {}'.format(device['name'], device['tags']['ck_channel_name'][str(outlet)])
-
-                    self._attributes['outlet_name'] = device['tags']['ck_channel_name'][str(outlet)]
-                else:
-                    self._name = '{} {}'.format(device['name'], ('CH %s' % str(outlet+1)) )
-            else:
-                self._name = '{} {}'.format(device['name'], ('CH %s' % str(outlet+1)) )
+            return self.deviceid
 
     @property
-    def is_on(self):
-        """Return true if device is on."""
-        self._state = self.get_state()
-        return self._state
-
-    def turn_on(self, **kwargs):
-        """Turn the device on."""
-        self._hass.bus.async_fire('sonoff_state', {
-            'state'     : True,
-            'deviceid'  : self._deviceid,
-            'outlet'    : self._outlet
-        })
-        self.async_schedule_update_ha_state()
-
-    def turn_off(self, **kwargs):
-        """Turn the device off."""
-        self._hass.bus.async_fire('sonoff_state', {
-            'state'     : False,
-            'deviceid'  : self._deviceid,
-            'outlet'    : self._outlet
-        })
-        self.async_schedule_update_ha_state()
-
-    # entity id is required if the name use other characters not in ascii
-    @property
-    def entity_id(self):
-        """Return the unique id of the switch."""
-
-        if self._hass.data[SONOFF_DOMAIN].get_entity_prefix():
-            entity_id = "{}.{}_{}".format(DOMAIN, SONOFF_DOMAIN, self._deviceid)
-        else:
-            entity_id = "{}.{}".format(DOMAIN, self._deviceid)
-
-        if self._outlet is not None:
-            entity_id = "{}_{}".format(entity_id, str(self._outlet+1))
-
-        return entity_id
-
-class SonoffDebugSwitch(SwitchDevice):
-    def __init__(self, hass):
-        self._hass = hass
+    def name(self) -> Optional[str]:
+        return self._name
 
     @property
-    def should_poll(self):
-        return False
+    def state_attributes(self):
+        return self._attrs
 
     @property
-    def name(self):
-        return "sonoff debug"
+    def available(self) -> bool:
+        device: dict = self.registry.devices[self.deviceid]
+        return device['available']
 
     @property
-    def available(self):
-        return True
-
-    def update(self):
-        pass
+    def supported_features(self):
+        return 0
 
     @property
-    def entity_id(self):
-        entity_id = "{}.{}".format(DOMAIN, 'sonoff_debug')
-        return entity_id
+    def is_on(self) -> bool:
+        return self._is_on
 
-    @property
-    def is_on(self):
-        return self._hass.states.get('switch.sonoff_debug') and \
-                self._hass.states.is_state('switch.sonoff_debug','on')
+    async def async_turn_on(self, **kwargs) -> None:
+        await self._turn_on()
 
-    def turn_on(self, **kwargs):
-        self._hass.states.set('switch.sonoff_debug', 'on')
+    async def async_turn_off(self, **kwargs) -> None:
+        await self._turn_off()
 
-    def turn_off(self, **kwargs):
-        self._hass.states.set('switch.sonoff_debug', 'off') # , attr
-        self._hass.data[SONOFF_DOMAIN].write_debug('{}') # send dummy data to trigger the notification
+    async def async_update(self):
+        """Auto refresh device state.
+
+        Called from: `EntityPlatform._update_entity_states`
+
+        https://github.com/AlexxIT/SonoffLAN/issues/14
+        """
+        _LOGGER.debug(f"Refresh device state {self.deviceid}")
+        params = {'sledonline': self._sled_online} \
+            if self._sled_online is not None else \
+            {'cmd': 'signal_strength'}
+        await self.registry.send(self.deviceid, params)
